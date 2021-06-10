ea6d7b905900d5c4b869f7b6c327e6b7b7b59dda
JobSindidi <JobSindidi@SindidiPc>
2021-06-10 09:27:40 +0000

    my fister commit

---
+++ GestionEmployer/src/main/java/org/employer/GesstionEmployer.java
@@ -0,0 +1,13 @@
+package org.employer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesstionEmployer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesstionEmployer.class, args);
+	}
+
+}
